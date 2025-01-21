d33655bf835a30124f5d18ccef7cc0b22958b99d
Ese-Tech <eseosagie22@gmail.com>
2025-01-21 12:27:44 +0100

    Refactor header component: update navigation links, implement dynamic loading, and add routing functionality

--- src/components/header.ts
+++ src/components/header.ts
@@ -1,14 +1,17 @@
+import { setupNavigation } from '../router';
+
 export function loadHeader() {
-    fetch('/src/components/header.html')
-      .then(response => response.text())
-      .then(data => {
-        const headerElement = document.getElementById('header');
-        if (headerElement) {
-          headerElement.innerHTML = data;
-        } else {
-          console.error('Header element not found');
-        }
-        // Add any additional JavaScript functionality for the header here
-      })
-      .catch(error => console.error('Error loading header:', error));
-  }
+  fetch('/src/components/header.html')
+    .then(response => response.text())
+    .then(data => {
+      const headerElement = document.getElementById('header');
+      if (headerElement) {
+        headerElement.innerHTML = data;
+        setupNavigation(); // Call setupNavigation after the header is loaded
+      } else {
+        console.error('Header element not found');
+      }
+      // Add any additional JavaScript functionality for the header here
+    })
+    .catch(error => console.error('Error loading header:', error));
+}
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
-import './styles/index.css';
-import { loadHeader } from './components/header';
+import "./styles/index.css";
+import { loadHeader } from "./components/header";
+import { navigateTo } from "./router";
 
-document.addEventListener('DOMContentLoaded', () => {
-  loadHeader();
+document.addEventListener("DOMContentLoaded", () => {
+	loadHeader();
+	navigateTo(window.location.pathname);
 });
