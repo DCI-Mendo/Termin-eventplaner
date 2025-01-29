63b7f285085c1a2a7c1a7dc7a5a238de751c063e
Ese-Tech <eseosagie22@gmail.com>
2025-01-29 23:10:11 +0100

    Refactor routing structure to use an array of route objects and update header links for services

--- features/routes.ts
+++ features/routes.ts
@@ -1,13 +1,39 @@
-import { initContactPage } from "../src/pages/contact";
-import { openPopup, closePopup } from "../src/components/popup";
-import { EventRenderer } from "../src/pages/eventBooking"; // Import EventRenderer
-
-const routes: { [key: string]: string } = {
-  "/": "home.html",
-  "/calendar": "calendar.html",
-  "/services": "eventsBooking.html",
-  "/about": "about.html",
-  "/contact": "contact.html",
-};
+import homePage from "../src/pages/home";
+import contactPage from "../src/pages/contact";
+import notFoundPage from "../pages/notFound";
+import aboutPage from "../src/pages/about";
+import EventsPage from "../src/data/events";
+import servicesPage from "../src/pages/eventBooking";
+import calendarPage from "../src/pages/calendar";
+const routes = [
+  {
+    path: "/",
+    page: homePage,
+  },
+  {
+    path: "/calendar",
+    page: calendarPage,
+  },
+  {
+    path: "/eventsBooking/",
+    page: servicesPage,
+  },
+  {
+    path: "/events/:id",
+    page: EventsPage,
+  },
+  {
+    path: "/about",
+    page: aboutPage,
+  },
+  {
+    path: "/contact",
+    page: contactPage,
+  },
+  {
+    path: "*",
+    page: notFoundPage,
+  },
+];
 
 export { routes };
