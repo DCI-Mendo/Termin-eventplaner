643cfced005fbd4308bec7231c5398fe2215eada
Ese-Tech <eseosagie22@gmail.com>
2025-01-30 18:14:01 +0100

    Implement dynamic footer loading and adjust layout for improved spacing

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
 import "./styles/index.css";
 import { loadHeader } from "./components/header";
 import { navigateTo } from "../features/router";
+import { FooterManager } from "./components/footer";
 
-const initApp = () => {
+const initApp = async () => {
   loadHeader();
+  await loadFooter(); // Load the footer HTML
+  new FooterManager(); // Instantiate FooterManager
   navigateTo(window.location.pathname);
+};
+
+const loadFooter = async () => {
+  const response = await fetch("/src/components/footer.html");
+  const footerHTML = await response.text();
+  document.body.insertAdjacentHTML("beforeend", footerHTML);
 };
 
 initApp();
